26ba48dc0531f7db5fc7e7e215e708ad9199c590
Tobias Tobias Agyasta Halomoan <I464023@fhict.nl>
2021-01-16 04:55:43 +0100

    Added updated interim files

---
+++ Updated Interim/StudentPage.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCB_S04_Grp3_Project_TobiasHalomoan4252861
+{
+    public partial class StudentPageForm : Form
+    {
+        Student aStudent;
+        Tenant aTenant;
+        public StudentPageForm()
+        {
+            
+            InitializeComponent();
+            aStudent = new Student("Toby");
+
+        }
+
+        public StudentPageForm(Student childStudent)
+        {
+            InitializeComponent();
+            aStudent = childStudent;
+            UpdateForm();
+        }
+
+        public StudentPageForm(Tenant tenant)
+        {
+            InitializeComponent();
+            aStudent = new Student("Toby");
+            aTenant = tenant;
+            AddTenantData();
+            UpdateForm();
+        }
+
+        private void AddTenantData()
+        {
+            string[] RuleValues = aTenant.ReturnRules().Split('\n');
+            string[] EventValues = aTenant.ReturnEvents().Split('\n');
+            string[] ComplaintValues = aTenant.ReturnComplaints().Split('\n');
+
+            if (aTenant.ReturnRules() != string.Empty)
+            {
+                foreach (string Rule in RuleValues)
+                {
+                    if (Rule != string.Empty)
+                    {
+                        aStudent.SetRules(Rule);
+                    }
+                }
+            }
+            if (aTenant.ReturnEvents() != string.Empty)
+            {
+                foreach (string Event in EventValues)
+                {
+                    if (Event != string.Empty)
+                    {
+                        aStudent.SetEvents(Event);
+                    }
+                }
+            }
+            if (aTenant.ReturnComplaints() != string.Empty)
+            {
+                foreach (string Complaint in ComplaintValues)
+                {
+                    if (Complaint != string.Empty)
+                    {
+                        aStudent.SetComplaints(Complaint);
+                    }
+                }
+            }
+        }
+        private void UpdateForm()
+        {
+            StudentNameLbl.Text = aStudent.ReturnName();
+            if (aStudent.ReturnEvents() != string.Empty)
+            {
+                string[] eventvalues = aStudent.ReturnEvents().Split('\n');
+                foreach (string value in eventvalues)
+                {
+                    if (value != string.Empty)
+                    {
+                        eventsLB.Items.Add(value + " - " + aStudent.ReturnName());
+                    }
+                }
+
+            }
+            if (aStudent.ReturnComplaints() != string.Empty)
+            {
+                string[] complaintvalues = aStudent.ReturnComplaints().Split('\n');
+                foreach (string value in complaintvalues)
+                {
+                    if (value != string.Empty)
+                    {
+                        complaintsLB.Items.Add(value);
+                    } 
+                }
+            }
+            if (aStudent.ReturnRules() != string.Empty)
+            {
+                string[] rulevalues = aStudent.ReturnRules().Split('\n');
+                foreach (string value in rulevalues)
+                {
+                    if (value != string.Empty)
+                    {
+                        rulesLB.Items.Add(value);
+                    }
+                }
+            }
+        }
+
+        private void SubmissionBtn_Click(object sender, EventArgs e)
+        {
+            Submission_Form new_submit = new Submission_Form(aStudent);
+            new_submit.Show();
+            this.Hide();
+        }
+
+        private void btnTenant_Click(object sender, EventArgs e)
+        {
+          TenantForm tenant = new TenantForm(aStudent);
+          tenant.Show();
+          this.Close();
+        }
+    }
+}
---
+++ Updated Interim/TenantForm.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace PCB_S04_Grp3_Project_TobiasHalomoan4252861
+{
+    partial class TenantForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tbEvents = new System.Windows.Forms.TextBox();
+            this.btnRemoveRules = new System.Windows.Forms.Button();
+            this.lbEvent = new System.Windows.Forms.Label();
+            this.lbRule = new System.Windows.Forms.Label();
+            this.btnRemoveEvent = new System.Windows.Forms.Button();
+            this.btwnAddEvent = new System.Windows.Forms.Button();
+            this.lsbEvents = new System.Windows.Forms.ListBox();
+            this.btnAddRule = new System.Windows.Forms.Button();
+            this.tbRules = new System.Windows.Forms.TextBox();
+            this.lsbRules = new System.Windows.Forms.ListBox();
+            this.lsbComplaints = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lbComplaints = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRemoveWarning = new System.Windows.Forms.Button();
+            this.btnAddWarning = new System.Windows.Forms.Button();
+            this.btnRemoveStudent = new System.Windows.Forms.Button();
+            this.lsbStudents = new System.Windows.Forms.ListBox();
+            this.btnStudentPage = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1080, 568);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnStudentPage);
+            this.tabPage1.Controls.Add(this.tbEvents);
+            this.tabPage1.Controls.Add(this.btnRemoveRules);
+            this.tabPage1.Controls.Add(this.lbEvent);
+            this.tabPage1.Controls.Add(this.lbRule);
+            this.tabPage1.Controls.Add(this.btnRemoveEvent);
+            this.tabPage1.Controls.Add(this.btwnAddEvent);
+            this.tabPage1.Controls.Add(this.lsbEvents);
+            this.tabPage1.Controls.Add(this.btnAddRule);
+            this.tabPage1.Controls.Add(this.tbRules);
+            this.tabPage1.Controls.Add(this.lsbRules);
+            this.tabPage1.Controls.Add(this.lsbComplaints);
+            this.tabPage1.Controls.Add(this.btnRemove);
+            this.tabPage1.Controls.Add(this.lbComplaints);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(1072, 539);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tbEvents
+            // 
+            this.tbEvents.Location = new System.Drawing.Point(679, 422);
+            this.tbEvents.Margin = new System.Windows.Forms.Padding(4);
+            this.tbEvents.Name = "tbEvents";
+            this.tbEvents.Size = new System.Drawing.Size(275, 22);
+            this.tbEvents.TabIndex = 13;
+            // 
+            // btnRemoveRules
+            // 
+            this.btnRemoveRules.Location = new System.Drawing.Point(212, 454);
+            this.btnRemoveRules.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemoveRules.Name = "btnRemoveRules";
+            this.btnRemoveRules.Size = new System.Drawing.Size(100, 28);
+            this.btnRemoveRules.TabIndex = 12;
+            this.btnRemoveRules.Text = "Remove";
+            this.btnRemoveRules.UseVisualStyleBackColor = true;
+            this.btnRemoveRules.Click += new System.EventHandler(this.btnRemoveRules_Click_1);
+            // 
+            // lbEvent
+            // 
+            this.lbEvent.AutoSize = true;
+            this.lbEvent.BackColor = System.Drawing.Color.Transparent;
+            this.lbEvent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEvent.Location = new System.Drawing.Point(672, 273);
+            this.lbEvent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbEvent.Name = "lbEvent";
+            this.lbEvent.Size = new System.Drawing.Size(104, 31);
+            this.lbEvent.TabIndex = 11;
+            this.lbEvent.Text = "Events";
+            // 
+            // lbRule
+            // 
+            this.lbRule.AutoSize = true;
+            this.lbRule.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRule.Location = new System.Drawing.Point(33, 31);
+            this.lbRule.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbRule.Name = "lbRule";
+            this.lbRule.Size = new System.Drawing.Size(78, 31);
+            this.lbRule.TabIndex = 10;
+            this.lbRule.Text = "rules";
+            // 
+            // btnRemoveEvent
+            // 
+            this.btnRemoveEvent.Location = new System.Drawing.Point(825, 454);
+            this.btnRemoveEvent.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemoveEvent.Name = "btnRemoveEvent";
+            this.btnRemoveEvent.Size = new System.Drawing.Size(129, 46);
+            this.btnRemoveEvent.TabIndex = 9;
+            this.btnRemoveEvent.Text = "Remove";
+            this.btnRemoveEvent.UseVisualStyleBackColor = true;
+            this.btnRemoveEvent.Click += new System.EventHandler(this.btnRemoveEvent_Click_1);
+            // 
+            // btwnAddEvent
+            // 
+            this.btwnAddEvent.Location = new System.Drawing.Point(679, 454);
+            this.btwnAddEvent.Margin = new System.Windows.Forms.Padding(4);
+            this.btwnAddEvent.Name = "btwnAddEvent";
+            this.btwnAddEvent.Size = new System.Drawing.Size(129, 46);
+            this.btwnAddEvent.TabIndex = 8;
+            this.btwnAddEvent.Text = "Add";
+            this.btwnAddEvent.UseVisualStyleBackColor = true;
+            this.btwnAddEvent.Click += new System.EventHandler(this.btwnAddEvent_Click);
+            // 
+            // lsbEvents
+            // 
+            this.lsbEvents.FormattingEnabled = true;
+            this.lsbEvents.ItemHeight = 16;
+            this.lsbEvents.Location = new System.Drawing.Point(679, 308);
+            this.lsbEvents.Margin = new System.Windows.Forms.Padding(4);
+            this.lsbEvents.Name = "lsbEvents";
+            this.lsbEvents.Size = new System.Drawing.Size(275, 116);
+            this.lsbEvents.TabIndex = 7;
+            // 
+            // btnAddRule
+            // 
+            this.btnAddRule.Location = new System.Drawing.Point(212, 400);
+            this.btnAddRule.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddRule.Name = "btnAddRule";
+            this.btnAddRule.Size = new System.Drawing.Size(100, 28);
+            this.btnAddRule.TabIndex = 6;
+            this.btnAddRule.Text = "Add Rule";
+            this.btnAddRule.UseVisualStyleBackColor = true;
+            this.btnAddRule.Click += new System.EventHandler(this.btnAddRule_Click_1);
+            // 
+            // tbRules
+            // 
+            this.tbRules.Location = new System.Drawing.Point(40, 400);
+            this.tbRules.Margin = new System.Windows.Forms.Padding(4);
+            this.tbRules.Name = "tbRules";
+            this.tbRules.Size = new System.Drawing.Size(132, 22);
+            this.tbRules.TabIndex = 5;
+            // 
+            // lsbRules
+            // 
+            this.lsbRules.FormattingEnabled = true;
+            this.lsbRules.ItemHeight = 16;
+            this.lsbRules.Location = new System.Drawing.Point(40, 65);
+            this.lsbRules.Margin = new System.Windows.Forms.Padding(4);
+            this.lsbRules.Name = "lsbRules";
+            this.lsbRules.Size = new System.Drawing.Size(287, 308);
+            this.lsbRules.TabIndex = 4;
+            // 
+            // lsbComplaints
+            // 
+            this.lsbComplaints.FormattingEnabled = true;
+            this.lsbComplaints.ItemHeight = 16;
+            this.lsbComplaints.Location = new System.Drawing.Point(679, 65);
+            this.lsbComplaints.Margin = new System.Windows.Forms.Padding(4);
+            this.lsbComplaints.Name = "lsbComplaints";
+            this.lsbComplaints.Size = new System.Drawing.Size(275, 116);
+            this.lsbComplaints.TabIndex = 2;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(679, 213);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(276, 46);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lbComplaints
+            // 
+            this.lbComplaints.AutoSize = true;
+            this.lbComplaints.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbComplaints.Location = new System.Drawing.Point(672, 31);
+            this.lbComplaints.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbComplaints.Name = "lbComplaints";
+            this.lbComplaints.Size = new System.Drawing.Size(160, 31);
+            this.lbComplaints.TabIndex = 1;
+            this.lbComplaints.Text = "Complaints";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.btnRemoveWarning);
+            this.tabPage2.Controls.Add(this.btnAddWarning);
+            this.tabPage2.Controls.Add(this.btnRemoveStudent);
+            this.tabPage2.Controls.Add(this.lsbStudents);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(1072, 539);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Students";
+            // 
+            // btnRemoveWarning
+            // 
+            this.btnRemoveWarning.BackColor = System.Drawing.Color.Green;
+            this.btnRemoveWarning.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRemoveWarning.Location = new System.Drawing.Point(427, 251);
+            this.btnRemoveWarning.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemoveWarning.Name = "btnRemoveWarning";
+            this.btnRemoveWarning.Size = new System.Drawing.Size(135, 48);
+            this.btnRemoveWarning.TabIndex = 3;
+            this.btnRemoveWarning.Text = "Remove Warning";
+            this.btnRemoveWarning.UseVisualStyleBackColor = false;
+            // 
+            // btnAddWarning
+            // 
+            this.btnAddWarning.BackColor = System.Drawing.Color.Red;
+            this.btnAddWarning.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAddWarning.Location = new System.Drawing.Point(427, 145);
+            this.btnAddWarning.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddWarning.Name = "btnAddWarning";
+            this.btnAddWarning.Size = new System.Drawing.Size(135, 48);
+            this.btnAddWarning.TabIndex = 2;
+            this.btnAddWarning.Text = "Warn";
+            this.btnAddWarning.UseVisualStyleBackColor = false;
+            // 
+            // btnRemoveStudent
+            // 
+            this.btnRemoveStudent.BackColor = System.Drawing.Color.Red;
+            this.btnRemoveStudent.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRemoveStudent.Location = new System.Drawing.Point(427, 55);
+            this.btnRemoveStudent.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemoveStudent.Name = "btnRemoveStudent";
+            this.btnRemoveStudent.Size = new System.Drawing.Size(135, 48);
+            this.btnRemoveStudent.TabIndex = 1;
+            this.btnRemoveStudent.Text = "Remove";
+            this.btnRemoveStudent.UseVisualStyleBackColor = false;
+            // 
+            // lsbStudents
+            // 
+            this.lsbStudents.FormattingEnabled = true;
+            this.lsbStudents.ItemHeight = 16;
+            this.lsbStudents.Location = new System.Drawing.Point(41, 55);
+            this.lsbStudents.Margin = new System.Windows.Forms.Padding(4);
+            this.lsbStudents.Name = "lsbStudents";
+            this.lsbStudents.Size = new System.Drawing.Size(341, 420);
+            this.lsbStudents.TabIndex = 0;
+            // 
+            // btnStudentPage
+            // 
+            this.btnStudentPage.Location = new System.Drawing.Point(392, 436);
+            this.btnStudentPage.Name = "btnStudentPage";
+            this.btnStudentPage.Size = new System.Drawing.Size(202, 63);
+            this.btnStudentPage.TabIndex = 14;
+            this.btnStudentPage.Text = "Student Page";
+            this.btnStudentPage.UseVisualStyleBackColor = true;
+            this.btnStudentPage.Click += new System.EventHandler(this.btnStudentPage_Click);
+            // 
+            // TenantForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 568);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "TenantForm";
+            this.Text = "TenantForm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox tbEvents;
+        private System.Windows.Forms.Button btnRemoveRules;
+        private System.Windows.Forms.Label lbEvent;
+        private System.Windows.Forms.Label lbRule;
+        private System.Windows.Forms.Button btnRemoveEvent;
+        private System.Windows.Forms.Button btwnAddEvent;
+        private System.Windows.Forms.ListBox lsbEvents;
+        private System.Windows.Forms.Button btnAddRule;
+        private System.Windows.Forms.TextBox tbRules;
+        private System.Windows.Forms.ListBox lsbRules;
+        private System.Windows.Forms.ListBox lsbComplaints;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lbComplaints;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRemoveWarning;
+        private System.Windows.Forms.Button btnAddWarning;
+        private System.Windows.Forms.Button btnRemoveStudent;
+        private System.Windows.Forms.ListBox lsbStudents;
+        private System.Windows.Forms.Button btnStudentPage;
+    }
+}
